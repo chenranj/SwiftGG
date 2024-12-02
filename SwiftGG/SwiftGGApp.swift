06bf746f0fe22b188a6702903b6f391dbb7ef368
Chenran Jin <hello@jin.cr>
2024-12-02 18:42:24 -0500

    Initial Commit

---
+++ SwiftGG/SwiftGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGGApp.swift
+//  SwiftGG
+//
+//  Created by Chenran Jin on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
